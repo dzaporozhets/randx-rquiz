4cf6c7d5765def16d1405ccdabf9cb9eb12c4315
Dmitriy Zaporozhets <dzaporozhets@sphereconsultinginc.com>
2011-05-11 16:32:53 +0300

    Quiz updated. 3 failures

--- 100/quiz100.rb
+++ 100/quiz100.rb
@@ -2,6 +2,14 @@ require "rubygems"
 require "awesome_print"
 
 module Compiler
+  CONST = 0x01
+  LCONST = 0x02
+  ADD = 0x0a
+  SUB = 0x0b
+  MUL = 0x0c
+  POW = 0x0d
+  DIV = 0x0e
+
   class << self 
     def compile(expr)
       bytecode = []
@@ -12,7 +20,7 @@ module Compiler
     end
 
     def parse(expr)
-      items = expr.split(%r{(\d*)\s*(\d*)}).reject(&:empty?)
+      items = expr.split(%r{(\d)\s*(\d*)}).reject(&:empty?)
       items.each_slice(3).each do |array|
         yield(to_code(array[0]), to_code(array[2]), to_code(array[1]))
       end
@@ -21,22 +29,38 @@ module Compiler
 
     def to_code(str)
       case str
-      when "+" then 10
-      when "-" then 11
-      when "*" then 12
-      when "/" then 13
+      when "+" then ADD
+      when "-" then SUB
+      when "*" then MUL
+      when "/" then DIV
+      when "**" then POW
       else 
         digit = str.to_i
-        to_long_const(digit)
+        digit_to_bytes(digit)
       end
     end
 
-    def to_long_const(digit)
-      [2, 0, 0, 0, digit]
+    def digit_to_bytes(digit)
+      bytes = []
+      x_to_256(digit, bytes)
+      array = bytes.reverse
+      array.slice!(4, 10)
+      if array.size > 2
+        array.insert(0, 0) until array.size >= 4
+        [LCONST, array].flatten
+      else 
+        array.insert(0, 0) until array.size >= 2
+        [CONST, array].flatten
+      end
     end
 
-    def to_small_const(digit)
-      [1, 0, digit]
+    def x_to_256(digit, bytes)
+      if digit > 256
+        bytes << 255
+        x_to_256(digit/256, bytes)
+      else
+        bytes << digit
+      end
     end
   end
 end
